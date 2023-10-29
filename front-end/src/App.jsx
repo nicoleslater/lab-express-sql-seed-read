d51cac014b82d30b298c767cbccea243c6cea7fb
Nicole Slater <nicoleslater@pursuit.org>
2023-10-29 17:07:52 -0400

    Updating Components and Pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,31 @@
+import {BrowserRouter as Router, Routes, Routes} from "react-router-dom";
+
+
+
+
+
+
+
+
+
 function App() {
   return (
-    <div className="">
+    <div className="App">
       <h1>Tuner App</h1>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/songs" element={<Index />} />
+            <Route path="/songs/new" element={<New />} />
+            <Route exact path="/songs/:id" element={<Show />} />
+            <Route path="/songs/:id/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+      
     </div>
   );
 }
