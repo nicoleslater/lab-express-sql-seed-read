8c7f36a0600bc083b27a02f819f63a1a6704a836
Nicole Slater <nicoleslater@pursuit.org>
2023-10-29 14:10:40 -0400

    Run psql schema and seed, route/songs is not working

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,6 +2,6 @@
 
 INSERT INTO songs (name, artist, album, time, is_favorite)
 VALUES
-    ('Endless Love', 'Luther Vandross & Mariah Carey', 'Songs', '4:21', true),
-    ('I Will Always Love You', 'Whitney Houston', 'The Bodyguard Soundtrack', '4:31', true),
-    ('No Diggity', 'Blackstreet ft. Dr. Dre', 'Another Level', '4:14', true);
+    ('Endless Love', 'Luther Vandross & Mariah Carey', 'Songs', 421, true),
+    ('I Will Always Love You', 'Whitney Houston', 'The Bodyguard Soundtrack', 431, true),
+    ('No Diggity', 'Blackstreet ft. Dr. Dre', 'Another Level', 414, true);
